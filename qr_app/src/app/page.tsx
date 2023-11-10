28a7315b4967d8e3f857b74eb0be076d0a142326
Itrkine Abdellah <48138349+Abdellah-Itrkine@users.noreply.github.com>
2023-11-10 16:58:09 +0100

    i created the qr app and add components folder with some files to use

---
+++ qr_app/src/app/page.tsx
@@ -0,0 +1,14 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function Home() {
+  const styles = {
+    color:'hsl(212, 45%, 89%)',
+  };
+  return (
+    
+    <body style={styles}> 
+
+    </body>
+  )
+}
---
+++ qr_app/src/components/QrCard.jsx
@@ -0,0 +1,12 @@
+
+
+
+export const  QrCard = () => {
+    return (
+        <div>
+                <p>QR image</p>
+                <p>QR text</p>
+
+        </div>
+        )
+    };
