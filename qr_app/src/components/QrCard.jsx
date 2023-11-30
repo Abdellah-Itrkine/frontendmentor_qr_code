a71546a7d78a6a6715afb915531cfb0cd583b4d0
Itrkine Abdellah <15321377-ItrkineAbdelllah@users.noreply.gitlab.com>
2023-11-30 15:01:22 +0100

    remove action and buttons

--- qr_app/src/components/QrCard.jsx
+++ qr_app/src/components/QrCard.jsx
@@ -5,8 +5,6 @@ import {ImgMediaCard} from './QrTextBox'
 export const  QrCard = () => {
     return (
         <div>
-            <p>QR image</p>
-            <p>QR text</p>
             <ImgMediaCard />
         </div>
         )
